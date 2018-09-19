11990696b8cd170dcfebcbd460f60e127c223285
siqqQ <b.bohosyan@gmail.com>
2018-09-19 14:41:54 +0300

    Added No.182 Solution

--- LeetCode.sql
+++ LeetCode.sql
@@ -5,3 +5,6 @@ where area > 3000000 or population > 25000000;
 select * from cinema
 where id%2 = 1 and description <> "boring"
 order by rating desc;
+-- 182. Duplicate Emails
+select distinct p1.Email from Person p1
+where (select count(p2.Id) from Person p2 where p2.Email = p1.Email having count(p2.Id) > 1);
