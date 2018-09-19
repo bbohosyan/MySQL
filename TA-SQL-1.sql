c8d470b411f2f533e3f3eb5c9f627f109efd29d4
siqqQ <b.bohosyan@gmail.com>
2018-09-19 14:28:27 +0300

    Created sql file to store LeetCode database solutions and added No.595 Solution

--- TA-SQL-1.sql
+++ TA-SQL-1.sql
@@ -3,13 +3,9 @@ select * from telerikacademy.departments;
 -- 2 Write a SQL query to find all department names.
 select Name from telerikacademy.departments;
 -- 3 Write a SQL query to find the salary of each employee.
-select FirstName, LastName, Salary from telerikacademy.employees;
+select concat(FirstName,' ', ifnull(MiddleName, ''),' ', LastName) as FullName, Salary from telerikacademy.employees;
 -- 4 Write a SQL to find the full name of each employee.
-select concat(FirstName, ' ', LastName) as FullName from telerikacademy.employees;
 -- 5 Write a SQL query to find the email addresses of each employee (by his first and last name). Consider that the mail domain is telerik.com. Emails should look like “John.Doe@telerik.com". The produced column should be named "Full Email Addresses".
-select concat(FirstName, ' ', LastName) as `Full Name`, 
-concat(FirstName, '.', LastName, '@telerik.com') as `Email` 
-from telerikacademy.employees;
 -- 6 Write a SQL query to find all different employee salaries.
 select distinct Salary from telerikacademy.employees;
 -- 7 Write a SQL query to find all information about the employees whose job title is “Sales Representative“.
@@ -29,27 +25,23 @@ select * from telerikacademy.employees where Salary > 50000 order by Salary desc
 -- 14 Write a SQL query to find the top 5 best paid employees.
 select * from telerikacademy.employees order by Salary desc limit 5;
 -- 15 Write a SQL query to find all employees and their address.
-select e.FirstName, ifnull(e.MiddleName, '') 
-as MiddleName, e.LastName, a.AddressText from telerikacademy.employees e
+select * from telerikacademy.employees e
 join telerikacademy.addresses a on e.AddressID = a.AddressID;
 -- 16 Write a SQL query to find all employees that have manager, along with their manager.
-select e.FirstName, e.LastName, m.FirstName as ManagerFirstName, m.LastName 
-as ManagerLastName from telerikacademy.employees e
+select * from telerikacademy.employees e
 join telerikacademy.employees m on e.ManagerID = m.EmployeeID;
 -- 17 Write a SQL query to find all employees that have manager, along with their manager and their address.
-select e.FirstName, e.LastName, m.FirstName as ManagerFirstName, m.LastName 
-as ManagerLastName, a.AddressText from telerikacademy.employees e
+select * from telerikacademy.employees e
 join telerikacademy.employees m on e.ManagerID = m.EmployeeID
-join telerikacademy.addresses a on e.AddressID = a.AddressID;
+join telerikacademy.addresses a on e.AddressID = e.AddressID;
 -- 18 Write a SQL query to find all departments and all town names as a single list.
 select Name from telerikacademy.departments
-union
+union 
 select Name from telerikacademy.towns;
 -- 19 Write a SQL query to find all the employees and the manager for each of them along with the employees that do not have manager.
-select e.FirstName, e.LastName, m.FirstName as ManagerFirstName, m.LastName 
-as ManagerLastName from telerikacademy.employees e
+select * from telerikacademy.employees e
 left join telerikacademy.employees m on e.ManagerID = m.EmployeeID;
 -- 20 Write a SQL query to find the names of all employees from the departments "Sales" and "Finance" whose hire year is between 1995 and 2005.
-select e.FirstName, e.LastName, d.Name, e.HireDate from telerikacademy.employees e
+select * from telerikacademy.employees e
 join telerikacademy.departments d on e.DepartmentID = d.DepartmentID
-where Name in ("Sales", "Finance") and YEAR(e.HireDate) between 2002 and 2004;
+where d.Name in ("Sales", "Finance") and YEAR(HireDate) between 1995 and 2005;
