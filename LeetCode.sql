a6f4fd99ba7779a31d030b10d80ab90df681ccc9
siqqQ <b.bohosyan@gmail.com>
2018-09-19 14:35:46 +0300

    Added No.620 Solution

--- LeetCode.sql
+++ LeetCode.sql
@@ -1,3 +1,7 @@
 -- 595. Big Countries
 select name, population, area from World
-where area > 3000000 or population > 25000000
+where area > 3000000 or population > 25000000;
+-- 620. Not Boring Movies
+select * from cinema
+where id%2 = 1 and description <> "boring"
+order by rating desc;
